ddef821f4c89690bc931f8d24e5d0c57b09ba8a4
Sumit Rajput <grt.sumitr25@gmail.com>
2019-11-19 11:51:07 +0530

    Initial app setup

---
+++ app/app.js
@@ -0,0 +1,51 @@
+/**
+ * 
+ * AngularJS Reservation Calendar
+ * @description           Test Task
+ * @version               1.0
+ * @date                  November 2019
+ * 
+ */
+;(function() {
+
+
+  /**
+   * Definition of the main app module and its dependencies
+   */
+  angular
+    .module('reservation-calendar', [
+      'ngRoute'
+    ])
+    .config(config);
+
+  // safe dependency injection
+  // this prevents minification issues
+  config.$inject = ['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider'];
+
+  /**
+   * App routing
+   *
+   * You can leave it here in the config section or take it out
+   * into separate file
+   * 
+   */
+  function config($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
+
+    $locationProvider.html5Mode(false);
+  }
+
+  /**
+   * Run block
+   */
+  angular
+    .module('reservation-calendar')
+    .run(run);
+
+  run.$inject = ['$rootScope', '$location'];
+
+  function run($rootScope, $location) {
+
+    // put here everything that you need to run on page load
+
+  }
+})();
